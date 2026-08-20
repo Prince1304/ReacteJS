eda20b8ac701c2c8cc08d1cc671661e0b6157e66
Prince1304 <kyadaprince43@gmail.com>
2026-08-20 11:34:09 +0530

    write a app code on redux basic

--- Redux/start/src/App.jsx
+++ Redux/start/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './counterSlice';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  export default function Counter() {
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+  
+    return (
+      <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+        <h1>Count: {count}</h1>
+        <button onClick={() => dispatch(decrement())}>- Decrement</button>
+        <button onClick={() => dispatch(increment())} style={{ marginLeft: '10px' }}>
+          + Increment
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App

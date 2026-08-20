6449bdc2e8aab71d4d50a80fe71b6865644958b0
Prince1304 <kyadaprince43@gmail.com>
2026-08-20 11:34:31 +0530

    make full working redux code

---
+++ Redux/start/src/Redux/store.jsx
@@ -0,0 +1,9 @@
+import {configureStore} from '@reduxjs/toolkit'
+import counterReducer from '../Redux/Features/counter';
+
+ export const store = configureStore({
+    reducer:{
+
+    }
+}) 
+
--- Redux/start/src/main.jsx
+++ Redux/start/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import store from './Redux/store'
+import {Provider} from 'react-redux'
+import { store } from './Redux/store.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+    <StrictMode>
+        <Provider store={store}></Provider>
+            <App />
+    </StrictMode>
 )

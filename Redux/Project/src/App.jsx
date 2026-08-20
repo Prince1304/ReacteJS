efaa0cbfa6846dafa27f4fbaeefdd2da29953211
Prince1304 <kyadaprince43@gmail.com>
2026-08-20 14:53:25 +0530

    axios api call

--- Redux/Project/src/App.jsx
+++ Redux/Project/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import {pexels_api} from './API/api';
+import {axios} from 'axios';
 
 function App() {
   const respo = async ()=>{
-    let data = await fetch('https://api.pexels.com/v1/curated',
-      {params:('car',2,10)},
-      {Authentication:`User ${pexels_api}`}
-    )
+    let data = await axios.get('https://api.pexels.com/v1/search', {
+      // Axios lets us pass the query parameters cleanly as an object
+      params: {
+        query: 'car',
+        per_page: 10,
+        page: 2
+      },
+      headers: {
+        Authorization: pexels_api
+      }
+    })
     console.log(data);
   }
   respo()

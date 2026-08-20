36e5ef958166c5f1913876dca905561a31f89c9a
Prince1304 <kyadaprince43@gmail.com>
2026-08-20 11:50:06 +0530

    fix store code

--- Redux/start/src/Redux/store.jsx
+++ Redux/start/src/Redux/store.jsx
@@ -1,9 +1,9 @@
 import {configureStore} from '@reduxjs/toolkit'
 import counterReducer from '../Redux/Features/counter';
 
- export const store = configureStore({
-    reducer:{
-
-    }
-}) 
+export const store = configureStore({
+    reducer: {
+      counter: counterReducer,
+    },
+  });
 

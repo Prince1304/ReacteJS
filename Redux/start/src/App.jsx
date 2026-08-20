ee88796bb7ea0ad4e97873229070b77d6ce4eb73
Prince1304 <kyadaprince43@gmail.com>
2026-08-20 11:45:37 +0530

    add counter file

--- Redux/start/src/App.jsx
+++ Redux/start/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from './counterSlice';
+import Counter from './Counter';
 
 const App = () => {
   return(
-    
+    <Counter />
   )
 }
 

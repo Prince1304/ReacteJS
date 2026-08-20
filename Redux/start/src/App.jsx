b36d661ab49c95054a41c716797d260cfbc0480e
Prince1304 <kyadaprince43@gmail.com>
2026-08-20 11:47:28 +0530

    fix file import

--- Redux/start/src/App.jsx
+++ Redux/start/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './counterSlice';
+import { increment, decrement } from './Redux/Features/counter';
 import Counter from './Counter';
 
 const App = () => {

2fcff2161588f9d53b09af5f99be30a79610d13b
Prince1304 <kyadaprince43@gmail.com>
2026-08-21 12:45:30 +0530

    make a card

--- Redux/Reacte-Practice/Reacte-Practice/src/Components/Cards.jsx
+++ Redux/Reacte-Practice/Reacte-Practice/src/Components/Cards.jsx
@@ -3,8 +3,15 @@ import React from 'react'
 const Cards = () => {
   return (
     <>
-        <div className="container bg-amber-100 w-50 h-70 p-2 rounded-3xl">
-            <div className='container bg-amber-400 w-full h-full rounded-3xl'></div>
+        <div className="w-50 h-100 ">
+            <div className='w-10 h-70 ml-20 bg-amber-300 pt-70'></div>
+            <div className="w-10 h-10 rounded-t-full ml-20 border-t-5 border-b-0 border-amber-100 border-10"></div>
+            <div className="w-10 h-10 rounded-t-full ml-20 border-t-5 border-b-0 border-amber-100 border-10"></div>
+            <div className="container bg-amber-100 w-50 h-70 p-2 rounded-3xl">
+                <div className='container bg-amber-400 w-full h-full rounded-2xl'>
+
+                </div>
+            </div>
         </div>
     </>
   )

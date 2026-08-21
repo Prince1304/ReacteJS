991960710b77adacd2b16d1f798bdb3d6c9aaebe
Prince1304 <kyadaprince43@gmail.com>
2026-08-21 11:58:41 +0530

    make card container

--- Redux/Reacte-Practice/Reacte-Practice/src/Components/Cards.jsx
+++ Redux/Reacte-Practice/Reacte-Practice/src/Components/Cards.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Cards = () => {
   return (
     <>
-        <div className="container bg-amber-100">Data</div>
+        <div className="container bg-amber-100 w-50 h-70 p-2 rounded-3xl">
+            <div className='container bg-amber-400 w-full h-full rounded-3xl'></div>
+        </div>
     </>
   )
 }
